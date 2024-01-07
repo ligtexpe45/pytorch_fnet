df04cb66c9118594fb1225624cf9c50b00f7ecec
Mohamed Elsheikh <116109429+ligtexpe45@users.noreply.github.com>
2024-01-07 20:11:39 +0200

    Update bufferedpatchdataset.py

--- fnet/data/bufferedpatchdataset.py
+++ fnet/data/bufferedpatchdataset.py
@@ -1,175 +1,172 @@
-from collections import deque
-from typing import List, Sequence, Union
-import collections.abc
-import logging
-
-from tqdm import tqdm
+from fnet.data.fnetdataset import FnetDataset
 import numpy as np
 import torch
 
+from tqdm import tqdm
 
-logger = logging.getLogger(__name__)
-ArrayLike = Union[np.ndarray, torch.Tensor]
+import pdb
 
 
-class BufferedPatchDataset:
-    """Provides patches from items of a dataset.
+class BufferedPatchDataset(FnetDataset):
+    """Dataset that provides chunks/patchs from another dataset."""
 
-    Parameters
-    ----------
-    dataset
-        Dataset object.
-    patch_shape
-        Shape of patch to be extracted from dataset items.
-    buffer_size
-        Size of buffer.
-    buffer_switch_interval
-        Number of patches provided between buffer item exchanges. Set to -1 to
-        disable exchanges.
-    shuffle_images
-        Set to randomize order of dataset item insertion into buffer.
-
-    """
-
-    def __init__(
-        self,
-        dataset: collections.abc.Sequence,
-        patch_shape: Sequence[int] = (32, 64, 64),
-        buffer_size: int = 1,
-        buffer_switch_interval: int = -1,
-        shuffle_images: bool = True,
+    def __init__(self, 
+                 dataset,
+                 patch_size, 
+                 buffer_size = 1,
+                 buffer_switch_frequency = 720, 
+                 npatches = 100000,
+                 verbose = False,
+                 transform = None,
+                 shuffle_images = True,
+                 dim_squeeze = None,
     ):
-        self.dataset = dataset
-        self.patch_shape = patch_shape
-        self.buffer_size = min(len(self.dataset), buffer_size)
-        self.buffer_switch_interval = buffer_switch_interval
-        self.shuffle_images = shuffle_images
-
+        
         self.counter = 0
-        self.epochs = -1  # incremented to 0 when buffer initially filled
-        self.buffer = deque()
-        self.remaining_to_be_in_buffer = deque()
-        self.buffer_history = []
-        for _ in tqdm(range(self.buffer_size), desc="Buffering images"):
+        
+        self.dataset = dataset
+        self.transform = transform
+        
+        self.buffer_switch_frequency = buffer_switch_frequency
+        
+        self.npatches = npatches
+        
+        self.buffer = list()
+        
+        self.verbose = verbose
+        self.shuffle_images = shuffle_images
+        self.dim_squeeze = dim_squeeze
+        
+        shuffed_data_order = np.arange(0, len(dataset))
+
+        if self.shuffle_images:
+            np.random.shuffle(shuffed_data_order)
+        
+        
+        pbar = tqdm(range(0, buffer_size))
+                       
+        self.buffer_history = list()
+            
+        for i in pbar:
+            #convert from a torch.Size object to a list
+            if self.verbose: pbar.set_description("buffering images")
+
+            datum_index = shuffed_data_order[i]
+            datum = dataset[datum_index]
+            
+            datum_size = datum[0].size()
+            
+            self.buffer_history.append(datum_index)
+            self.buffer.append(datum)
+            
+        self.remaining_to_be_in_buffer = shuffed_data_order[i+1:]
+            
+        self.patch_size = [datum_size[0]] + patch_size
+
+            
+    def __len__(self):
+        return self.npatches
+
+    def __getitem__(self, index):
+        self.counter +=1
+        
+        if (self.buffer_switch_frequency > 0) and (self.counter % self.buffer_switch_frequency == 0):
+            if self.verbose: print("Inserting new item into buffer")
+                
             self.insert_new_element_into_buffer()
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        patch = self.get_random_patch()
-        self.counter += 1
-        if (self.buffer_switch_interval > 0) and (
-            self.counter % self.buffer_switch_interval == 0
-        ):
-            self.insert_new_element_into_buffer()
-        return patch
-
-    def _check_last_datum(self) -> None:
-        """Checks last dataset item added to buffer."""
-        nd = len(self.patch_shape)
-        idx_buf = self.buffer_history[-1]
-        shape_spatial = None
-        for idx_c, component in enumerate(self.buffer[-1]):
-            if shape_spatial is None:
-                shape_spatial = component.shape[-nd:]
-            elif component.shape[-nd:] != shape_spatial:
-                raise ValueError(
-                    f"Dataset item {idx_buf}, component {idx_c} shape "
-                    f"{component.shape} incompatible with first component "
-                    f"shape {self.buffer[-1][0].shape}"
-                )
-            if nd > len(component.shape) or any(
-                self.patch_shape[d] > shape_spatial[d] for d in range(nd)
-            ):
-                raise ValueError(
-                    f"Dataset item {idx_buf}, component {idx_c} shape "
-                    f"{component.shape} incompatible with patch_shape "
-                    f"{self.patch_shape}"
-                )
-
-    def insert_new_element_into_buffer(self) -> None:
-        """Inserts new dataset item into buffer.
-
-        Returns
-        -------
-        None
-
-        """
-        if len(self.remaining_to_be_in_buffer) == 0:
-            self.epochs += 1
-            self.remaining_to_be_in_buffer = deque(range(len(self.dataset)))
-            if self.shuffle_images:
+        
+        return self.get_random_patch()
+                       
+    def insert_new_element_into_buffer(self):
+        #sample with replacement
+                       
+        self.buffer.pop(0)
+        
+        if self.shuffle_images:
+            
+            if len(self.remaining_to_be_in_buffer) == 0:
+                self.remaining_to_be_in_buffer = np.arange(0, len(self.dataset))
                 np.random.shuffle(self.remaining_to_be_in_buffer)
-        if len(self.buffer) >= self.buffer_size:
-            self.buffer.popleft()
-        new_datum_index = self.remaining_to_be_in_buffer.popleft()
+            
+            new_datum_index = self.remaining_to_be_in_buffer[0]
+            self.remaining_to_be_in_buffer = self.remaining_to_be_in_buffer[1:]
+            
+        else:
+            new_datum_index = self.buffer_history[-1]+1
+            if new_datum_index == len(self.dataset):
+                new_datum_index = 0
+                             
         self.buffer_history.append(new_datum_index)
         self.buffer.append(self.dataset[new_datum_index])
-        logger.info(f"Added item {new_datum_index} into buffer")
-        self._check_last_datum()
+        
+        if self.verbose: print("Added item {0}".format(new_datum_index))
 
-    def get_random_patch(self) -> List[ArrayLike]:
-        """Samples random patch from an item in the buffer.
 
-        Let nd be the number of dimensions of the patch. If the item has more
-        dimensions than the patch, then sampling will be from the last nd
-        dimensions of the item.
-
-        Returns
-        -------
-        List[ArrayLike]
-            Random patch sampled from a dataset item.
-
-        """
-        nd = len(self.patch_shape)
+    def get_random_patch(self):
+        
         buffer_index = np.random.randint(len(self.buffer))
+                                   
         datum = self.buffer[buffer_index]
-        shape_spatial = datum[0].shape[-nd:]
-        patch = []
-        slices = None
-        for part in datum:
-            if slices is None:
-                starts = np.array(
-                    [
-                        np.random.randint(0, d - p + 1)
-                        for d, p in zip(shape_spatial, self.patch_shape)
-                    ]
-                )
-                ends = starts + np.array(self.patch_shape)
-                slices = tuple(slice(s, e) for s, e in zip(starts, ends))
-            # Pad slices with "slice(None)" if there are non-spatial dimensions
-            slices_pad = (slice(None),) * (len(part.shape) - len(shape_spatial))
-            patch.append(part[slices_pad + slices])
-        return patch
 
-    def get_batch(self, batch_size: int) -> Sequence[torch.Tensor]:
-        """Returns a batch of patches.
+        ##The following commented out code is the original way of randomly obtaining random patches, it is a bit confusing and causes errors when doing hyperspectral deep learning -Bryce
+        ###starts = np.array([np.random.randint(0, d - p + 1) if d - p + 1 >= 1 else 0 for d, p in zip(datum[0].size(), self.patch_size)])
+        ###ends = starts + np.array(self.patch_size)
+        ####thank you Rory for this weird trick
+        ###index = [slice(s, e) for s,e in zip(starts,ends)]
+        
+        ###patch = [d[tuple(index)] for d in datum]
+        
+        ##The following code is what I've written (hamfisted) to grab random patches, it's more brittle and will need adjusted if the patch_size/buffer_size is adjusted, but works for hs deep learning -Bryce
+        
+        #Set parameters here
+        patch_size = 256 #dimension of the patch to be sampled in x or y
+        spec_channels = 200 #number of channels in starting tiff
+        final_channels = 17 #number of channels in final tiff
+        
+        start_0 = np.random.randint(0, patch_size)
+        start_1 = np.random.randint(0, patch_size)
+        starts = np.array([start_0, start_1])
 
-        Parameters
-        ----------
-        batch_size
-            Number of patches in batch.
+        ends = starts + np.array([patch_size, patch_size])
 
-        Returns
-        -------
-        Sequence[torch.Tensor]
-            Batch of patches.
-
-        """
-        return tuple(
-            torch.tensor(np.stack(part))
-            for part in zip(*[next(self) for _ in range(batch_size)])
-        )
-
-    def get_buffer_history(self) -> List[int]:
-        """Returns a list of indices of dataset elements inserted into the
-        buffer.
-
-        Returns
-        -------
-        List[int]
-            Indices of dataset elements.
-
-        """
+        #thank you Rory for this weird trick
+        ##Rory's weird tricks are no match for my hamfisitng
+        index = [slice(s, e) for s,e in zip(starts,ends)]
+        
+        index.insert(0, slice(0, spec_channels))
+        patch = [(datum[0])[tuple(index)]]
+        if final_channels == 1:
+            index.pop(0)
+            patch = patch + [(datum[1])[tuple(index)]]
+        
+            if self.dim_squeeze is not None:
+                patch = [torch.squeeze(d, self.dim_squeeze) for d in patch]
+            return patch
+        else: #final_channels > 1
+            index.pop(0)
+            index.insert(0, slice(0, final_channels))
+            patch = patch + [(datum[1])[tuple(index)]]
+        
+            if self.dim_squeeze is not None:
+                patch = [torch.squeeze(d, self.dim_squeeze) for d in patch]
+            return patch
+    
+    def get_buffer_history(self):
         return self.buffer_history
+    
+def _test():
+    # dims_chunk = (2,3,4)
+    dims_chunk = (4,5)
+    ds_test = ChunkDatasetDummy(
+        None,
+        dims_chunk = dims_chunk,
+    )
+    print('Dataset len', len(ds_test))
+    for i in range(3):
+        print('***** {} *****'.format(i))
+        element = ds_test[i]
+        print(element[0])
+        print(element[1])
+    
+if __name__ == '__main__':
+    _test()
